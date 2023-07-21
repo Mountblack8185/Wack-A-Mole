47e2187b5dd342f0087d18c4d0960e9d5e7afb81
Anthony <Anthony@10.0.0.222>
2023-07-20 21:30:04 -0400

    Wack-A-Mole project submission attempt

---
+++ src/wack/a/mole/WackAMole.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package wack.a.mole;
+
+/**
+ *
+ * @author Anthony
+ */
+public class WackAMole {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
